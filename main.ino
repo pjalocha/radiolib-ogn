f829f5bc054d64b644eb04ad24840ca816bc6ec4
Pawel Jalocha <Pawel.Jalocha@gmail.com>
2022-04-18 14:49:30 +0100

    Add comments

--- main.ino
+++ main.ino
@@ -11,14 +11,14 @@
 #include "rfm.h"
 
 #ifdef HAS_SX1276
-static SX1276 Radio = new Module(LORA_CS, LORA_IRQ, LORA_RST, RADIOLIB_NC);
+static SX1276 Radio = new Module(LORA_CS, LORA_IRQ, LORA_RST, RADIOLIB_NC);  // create sx1276 RF module
 #endif
 
 #ifdef HAS_SX1262
-static SX1262 Radio = new Module(LORA_CS, LORA_IRQ, LORA_RST, LORA_BUSY);
+static SX1262 Radio = new Module(LORA_CS, LORA_IRQ, LORA_RST, LORA_BUSY);    // create sx1262 RF module
 #endif
 
-uint64_t UniqueID = 0;
+uint64_t UniqueID = 0; // unique CPU ID to create a unique radio address
 
 // OGNv1 SYNC:       0x0AF3656C encoded in Manchester
 static const uint8_t OGN1_SYNC[8] = { 0xAA, 0x66, 0x55, 0xA5, 0x96, 0x99, 0x96, 0x5A };
@@ -35,7 +35,7 @@ static void SetupOGN(int Channel=0, float TxPower=10)         // Radio setup for
   Radio.setCurrentLimit(100);                                 // [mA]   depends on transmission power
   Radio.setDataShaping(RADIOLIB_SHAPING_0_5);                 // [BT]   FSK modulation shaping 
   Radio.setPreambleLength(8);                                 // [bits] minimal preamble
-  Radio.setCRC(0, 0);                                         // 
+  Radio.setCRC(0, 0);                                         // disable CRC: we do it ourselves
   Radio.fixedPacketLengthMode(2*OGN_TxPacket<OGN1_Packet>::Bytes); // [bytes] Fixed packet size mode
   
   Radio.disableAddressFiltering();
@@ -48,7 +48,7 @@ static RFM_FSK_RxPktData         RxPktData;                          // received
 static LDPC_Decoder              Decoder;                            // decoder and error corrector for the OGN Gallager/LDPC code
 static OGN_RxPacket<OGN1_Packet> RxPacket;                           // received, decoded and corrected packet
 
-static void EncodePacket(OGN_TxPacket<OGN1_Packet> &TxPacket)
+static void EncodePacket(OGN_TxPacket<OGN1_Packet> &TxPacket)  // encode some fixed (demo) position of the aircraft
 {
   TxPacket.Packet.HeaderWord = 0;                  // start with clean header
   TxPacket.Packet.Header.Address=UniqueID&0xFFFFFF; // set address: 24-bit number
@@ -81,70 +81,68 @@ static void EncodePacket(OGN_TxPacket<OGN1_Packet> &TxPacket)
 static int Manchester(uint8_t *Out, const OGN_TxPacket<OGN1_Packet> &TxPacket) // Encode as Manchester
 { const uint8_t *Inp = TxPacket.Byte();
   int Len=0;
-  for(int Idx=0; Idx<TxPacket.Bytes; Idx++)
+  for(int Idx=0; Idx<TxPacket.Bytes; Idx++)        // loop over bytes and encode usinglookup table
   { uint8_t Byte=Inp[Idx];
     Out[Len++]=ManchesterEncode[Byte>>4];
     Out[Len++]=ManchesterEncode[Byte&0x0F]; }
   return Len; }                                    // returns number of bytes in the encoded packet
 
-void setup()
+void setup()                                       // Arduino convention: called once at startup
 {
-  UniqueID = ESP.getEfuseMac();
+  UniqueID = ESP.getEfuseMac();                    // get unique ID of the CPU/chipset
 
-  Serial.begin(115200);
+  Serial.begin(115200);                            // start serial console
   delay(200);
 
   Serial.println("RadioLib OGN-Tracker demo");
 
-  Radio.beginFSK(868.2, 100.0, 50.0, 250.0, 0, 8);    // RF in FSK mode (not LoRa)
+  Radio.beginFSK(868.2, 100.0, 50.0, 250.0, 0, 8); // start the RF chip in FSK mode (not LoRa)
 #ifdef HAS_SX1262
   Radio.setTCXO(2.4);
   Radio.setDio2AsRfSwitch();
 #endif
 }
 
-static int ReceiveLoop(int ListenTime)
-{ int state=Radio.startReceive(2*OGN_TxPacket<OGN1_Packet>::Bytes);
+static int ReceiveLoop(int ListenTime)             // receive/listen for packets for a given time period
+{ int state=Radio.startReceive(2*OGN_TxPacket<OGN1_Packet>::Bytes);  // enter reception mode (if it was idle)
   // printf("Radio.startReceive(%d) => %d\n", 2*OGN_TxPacket<OGN1_Packet>::Bytes, state);
-  // Radio.clearIrqStatus(); // protected call
   int Count=0;
-  uint32_t Start = millis();
+  uint32_t Start = millis();                       // [ms] record the start time to meaure the time period
   for( ; ; )
-  { uint32_t Time = millis()-Start;
-    if(Time>=ListenTime) break;
+  { uint32_t Time = millis()-Start;                //
+    if(Time>=ListenTime) break;                    // end the loop when end-of-time reached
     if(digitalRead(LORA_IRQ))                      // if IRQ is high => new packet received
     // if(Radio.getIrqStatus())                       // this actually reads all 16 IRQ flags
-    { float RSSI = Radio.getRSSI(true);                 // 
-      Radio.readData(RxBuffer, 2*OGN_TxPacket<OGN1_Packet>::Bytes);
+    { float RSSI = Radio.getRSSI(true);                 // read the RSSI, but we are too slow, maybe greater RSSI integration time - but not supported by RadioLib
+      Radio.readData(RxBuffer, 2*OGN_TxPacket<OGN1_Packet>::Bytes);   // read the packet data
       uint8_t PktIdx=0;
-      for(uint8_t Idx=0; Idx<OGN_TxPacket<OGN1_Packet>::Bytes; Idx++)                                     // loop over packet bytes
+      for(uint8_t Idx=0; Idx<OGN_TxPacket<OGN1_Packet>::Bytes; Idx++) // loop over packet bytes
       { uint8_t ByteH = RxBuffer[PktIdx++];
         ByteH = ManchesterDecode[ByteH]; uint8_t ErrH=ByteH>>4; ByteH&=0x0F; // decode manchester, detect (some) errors
         uint8_t ByteL = RxBuffer[PktIdx++];
-        ByteL = ManchesterDecode[ByteL]; uint8_t ErrL=ByteL>>4; ByteL&=0x0F;
+        ByteL = ManchesterDecode[ByteL]; uint8_t ErrL=ByteL>>4; ByteL&=0x0F; // second nibble
         RxPktData.Data[Idx]=(ByteH<<4) | ByteL;
         RxPktData.Err [Idx]=(ErrH <<4) | ErrL ; }
-      uint8_t DecErr = RxPktData.Decode(RxPacket, Decoder);                 // run through LDPC FEC decoder
-      printf("RX: %3.1fdBm LDPC:%d errors:%d\n", RSSI, DecErr, RxPacket.RxErr);
-      // Radio.clearIrqStatus();
-      Count++; }
+      uint8_t DecErr = RxPktData.Decode(RxPacket, Decoder);                  // run through LDPC FEC decoder
+      printf("RX: %3.1fdBm LDPC:%d => %d bit errors\n", RSSI, DecErr, RxPacket.RxErr); // DedErr should be zero, if not the LDPC did not converge
+      Count++; }                                   // count received packets
     delay(1); }
-  Radio.standby();
-  return Count; }
+  Radio.standby();                                 // end reception/listen period
+  return Count; }                                  // return number of packets received
 
-void loop()
+void loop()                                        // 
 {
   Serial.println("Time-slot");
 
   EncodePacket(TxPacket);                          // encode position/speed/other data in the OGN packet
   int Bytes=Manchester(ManchPacket, TxPacket);     // convert to Manchester
-  SetupOGN(0, 2);                                  // setup for OGN transmission on channel #0 (868.2MHz)
+  SetupOGN(0, 10);                                 // setup for OGN transmission/reception on channel #0 (868.2MHz) with 10dBm
   Radio.transmit(ManchPacket, Bytes);              // transmit the Manchester encoded packet on channel #0
-  ReceiveLoop(500);                                // wait and receive OGN packets
+  ReceiveLoop(500);                                // receive OGN packets for 0.5sec
 
   EncodePacket(TxPacket);                          // 
   Bytes=Manchester(ManchPacket, TxPacket);         //
-  SetupOGN(1, 2);                                  // setup for OGN transmission on channel #1 (868.4MHz)
+  SetupOGN(1, 10);                                 // setup for OGN transmission on channel #1 (868.4MHz)
   Radio.transmit(ManchPacket, Bytes);              //
   ReceiveLoop(500);
 
